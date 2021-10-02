2931791f19d02c0b23e4f019a8b0c18af0528b47
Congtrinhh <trinhquycong@gmail.com>
2021-10-02 17:14:17 +0700

    initialize

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// vì không lồng thẻ a bên ngoài từng project được
+$(".project").on("click", function () {
+	$(this).find(".link").trigger("click");
+});
+
+const sections = document.querySelectorAll("section[id]");
+function handleScrollToSections() {
+	if ($(window).scrollTop() >= $(".header").height() + 20) {
+		$(".header").addClass("scroll");
+	} else {
+		$(".header").removeClass("scroll");
+	}
+
+	const scrollY = window.scrollY;
+
+	sections.forEach((section) => {
+		let sectionHeight = section.offsetHeight;
+		let sectionTop = section.offsetTop - 250; // -50
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			$("header a[href*='" + section.id + "']").addClass("on");
+		} else {
+			$("header a[href*='" + section.id + "']").removeClass("on");
+		}
+	});
+}
+
+$(window).on("scroll", handleScrollToSections);
+
+$("#navToggle").on("click", function () {
+	const closeIcon = `<i class="uil uil-times"></i>`;
+	const openIcon = `<i class="uil uil-bars"></i>`;
+	$(this).siblings(".header ul").toggleClass("on");
+
+	if ($(this).siblings(".header ul").hasClass("on")) {
+		$(this).html(closeIcon);
+	} else {
+		$(this).html(openIcon);
+	}
+});
+
+$(".header li").on("click", function () {
+	$(".header ul").removeClass("on");
+	$("#navToggle").html(`<i class="uil uil-bars"></i>`);
+});
+
+// dùng media query
+function myFunction(x) {
+	if (x.matches) {
+		// thêm class để thành swiper
+		$(".projects > .container").addClass("swiper").addClass("projectsSwiper");
+		$(".projects > .container > .project-wrapper").addClass("swiper-wrapper").removeClass("row");
+		$(".projects > .container > .project-wrapper > div[class*='col']").addClass("swiper-slide");
+		$(".button-next").addClass("swiper-button-next");
+		$(".button-prev").addClass("swiper-button-prev");
+		$(".pagination").addClass("swiper-pagination");
+
+		// khởi tạo swiper
+		const swiper = new Swiper(".projectsSwiper", {
+			cssMode: true,
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			mousewheel: true,
+			keyboard: true,
+		});
+	} else {
+		console.log("nothing change");
+	}
+}
+
+var x = window.matchMedia("(max-width: 768px)");
+myFunction(x); // Call listener function at run time
